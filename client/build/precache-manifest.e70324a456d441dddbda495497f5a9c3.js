ab537eb54336d293b66c69d1ae581fc417b42b38
Skelman50 <skelman50@gmail.com>
2019-03-08 23:44:29 +0200

    prepare to deploy

---
+++ client/build/precache-manifest.e70324a456d441dddbda495497f5a9c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4404510f34dfffb9793e",
+    "url": "/static/js/main.4404510f.chunk.js"
+  },
+  {
+    "revision": "3c42b92c01c8ef682d2a",
+    "url": "/static/js/2.3c42b92c.chunk.js"
+  },
+  {
+    "revision": "4404510f34dfffb9793e",
+    "url": "/static/css/main.3a7d6df6.chunk.css"
+  },
+  {
+    "revision": "3c42b92c01c8ef682d2a",
+    "url": "/static/css/2.f8f66492.chunk.css"
+  },
+  {
+    "revision": "461cfed76fc00522bc4c28f63913009d",
+    "url": "/index.html"
+  }
+];
